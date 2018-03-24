9cd5807e2dcaced620f93049b26e5b6262e0043f
ClaireBvs <clairembeauvais@gmail.com>
2018-03-24 11:01:59 -0600

    test grid

--- test/test.rb
+++ test/test.rb
@@ -12,4 +12,18 @@ class BlahTest < Minitest::Test
     assert_instance_of Blah, @blah
   end
 
+  def test_braille_is_a_grid_of_cells
+    assert_instance of Array (or Hash)
+  end 
+    #every character returns 6 dots 2 columns 3 lines 2x3
+
+    # BUT capital letter = 4x3 because 2x3 + 2x3
+
+    #need empty column as separation ?
+
+  end
+
+
+
+
 end
