70154ea2d504faa593656a0767408e8c62a1d6f0
ClaireBvs <clairembeauvais@gmail.com>
2018-03-25 17:02:33 -0600

    new dictionary

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -6,21 +6,23 @@ require 'pry'
 class DictionaryTest < MiniTest::Test
 
   def test_it_exists
-    translate = Dictionary.new
-    assert_instance_of Dictionary, translate
+    dictionary = Dictionary.new
+    assert_instance_of Dictionary, dictionary
   end
 
   def test_it_translate_english_to_braille
-  skip
     dictionary = Dictionary.new
-    actual = translation.eng_to_braille_alpha{"e"}
-    expected = "000..."
+    actual = eng_to_braille("e")
+    expected = ["0.",
+                ".0",
+                ".."]
     assert_equal expected, actual
   end
 
   def test_it_translate_braille_to_english
+    skip
     dictionary = Dictionary.new
-    actual = version.braille_to_eng_alpha{"0.0..."}
+    actual = version.braille_to_eng_alpha("0.0...")
     expected = "b"
     assert equal expected, actual
   end
