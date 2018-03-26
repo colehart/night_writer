a180456cd0d545be9e1c56ff7e6525884e4f55cd
Cole Hart <cole@colehart.com>
2018-03-26 16:49:14 -0600

    reset HEAD to before tripler

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -8,9 +8,11 @@ class NightWrite
   end
 
   def display_confirmation_message
+    @writer.echo_plaintext_three_times
     @writer.write_encoded_message
     return "Created '#{@writer.encoded_filename}' containing #{@writer.byte_count} characters."
   end
+
 end
 
 night_write = NightWrite.new
