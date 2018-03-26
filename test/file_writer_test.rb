4be3ad09735f22179d787a992352ba89d7d94e0d
ClaireBvs <clairembeauvais@gmail.com>
2018-03-26 14:09:15 -0600

    wrote better test for byte count

--- test/file_writer_test.rb
+++ test/file_writer_test.rb
@@ -28,12 +28,42 @@ class FileWriterTest < Minitest::Test
     assert_equal actual, expected
   end
 
-  def test_it_captures_the_bytes_in_byte_count
+  def test_it_starts_byte_count_at_0
     #message.txt contains "Hello world" as message
     file_writer = FileWriter.new
-    actual = 11
-    expected = file_writer.write_encoded_message
+    actual = 0
+    expected = file_writer.byte_count
     assert_equal actual, expected
   end
+
+  def test_byte_count_changes_when_you_write_a_new_encoded_message
+    #message.txt contains "Hello world" as message
+    file_writer = FileWriter.new
+    actual = 0
+    expected = file_writer.write_encoded_message
+    refute_equal actual, expected
+  end
+
+=begin
+2. Echo Characters
+Your Braille-simulation file will need three lines of output for every one line of input. Start by outputting your input in three repeated rows.
+
+hello world
+Turns into
+
+hello world
+hello world
+hello world
+
+# big picture
+ 3 lines of output for every input
+
+ # smaller picture
+ choose a method
+ AND make sure to have a line break
+=end
+
+  # def test_
+  # end
 
 end
