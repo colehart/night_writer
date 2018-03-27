71171c8c64adc98e1011fadc23575d88bed21d02
Cole Hart <cole@colehart.com>
2018-03-27 17:45:27 -0600

    fixed previous errors.

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -37,7 +37,7 @@ We are grabbing plaintext_message from file, not from ARGV
   def test_it_can_translate_from_filereader
     #passing message.txt in command line
     translator = Translator.new
-    actual = [["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."], ["  ", "  ", "  "], [".0", "00", ".0"], ["0.", ".0", "0."], ["0.", "00", "0."], ["0.", "0.", "0."], ["00", ".0", ".."]]
+    actual = [["..0.", "..00", ".0.."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."], ["  ", "  ", "  "], [".0", "00", ".0"], ["0.", ".0", "0."], ["0.", "00", "0."], ["0.", "0.", "0."], ["00", ".0", ".."]]
     expected = translator.eng_to_braille
     assert_equal actual, expected
   end
